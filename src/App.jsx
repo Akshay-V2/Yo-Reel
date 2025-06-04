bfe1fa63528871b033c8dae49aa53d1ad66c404a
Akshay V V <akshcodes2008@gmail.com>
2025-06-04 22:00:08 +0530

    Init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='top-bar'>
+      <div className='top-button'>About</div>
+      <div className='top-button'>aksh</div>
+    </div>
+    {/* add a glitch text reveal and magnetic hover to these buttons */}
+    </>
+  )
+}
+
+export default App
