3c445b30ca4727e9726c3b5430860e8ce2688d08
Akshay V V <akshcodes2008@gmail.com>
2025-06-07 14:07:26 +0530

    added a nice and cool background and made it a bit responsive to mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import {useState} from 'react'
 import Ticker from './components/Ticker.jsx';
+import Silk from './components/Silk.jsx';
 import './App.css'
 
 import { DiceFiveIcon } from "@phosphor-icons/react";
@@ -20,6 +21,16 @@ function App() {
 
   return (
     <>
+    <div className='bg-wrapper'>
+      <div className='fade'></div>
+      <Silk
+        speed={5}
+        scale={1}
+        color="#ff8135"
+        noiseIntensity={5.3}
+        rotation={0}/>
+    </div>
+
     <div className='top-bar'>
       <div className='top-button'>About</div>
       <div className='top-button'>aksh</div>
@@ -40,9 +51,12 @@ function App() {
 
     <Ticker digits={digits} />
     {/* add motion blur */}
-
     </>
   )
 }
 
 export default App
+
+// make a new rolling system in which the digits all roll to the lowest number and the time taken to role is programatically set based on the distance to the lowest number. (linear timing function)
+// then the numbers will switch to the highest number and then go to the target number. (cubic-out timing function)
+// add a sound effect when rolled.
